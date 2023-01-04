a2cc8254e21e9c959bc0d068b870e1481b8cab34
John Oerter <john@johnoerter.me>
2023-01-03 21:42:50 -0500

    green(temperature-converter): add text boxes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ function App() {
     <div>
       <h1>7 GUIs</h1>
       <Counter />
+      <h2>Temperature Converter</h2>
+      <form>
+        <input type="text" id="celsius" />
+        <label htmlFor="celsius">Celsius</label>
+        =
+        <input type="text" id="fahrenheit" />
+        <label htmlFor="fahrenheit">Fahrenheit</label>
+      </form>
     </div>
   );
 }
